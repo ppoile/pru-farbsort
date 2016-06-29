06d6108e2b6fb205b8804d5c1c0bdf6f72048920
Urs Fässler <urs.fassler@bbv.ch>
2016-06-29 16:10:26 +0200

    simple logger test

---
+++ logger/main.c
@@ -0,0 +1,200 @@
+/*
+ * Copyright (C) 2015 Texas Instruments Incorporated - http://www.ti.com/
+ *
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 	* Redistributions of source code must retain the above copyright
+ * 	  notice, this list of conditions and the following disclaimer.
+ *
+ * 	* Redistributions in binary form must reproduce the above copyright
+ * 	  notice, this list of conditions and the following disclaimer in the
+ * 	  documentation and/or other materials provided with the
+ * 	  distribution.
+ *
+ * 	* Neither the name of Texas Instruments Incorporated nor the names of
+ * 	  its contributors may be used to endorse or promote products derived
+ * 	  from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdint.h>
+#include <pru_cfg.h>
+#include <pru_intc.h>
+#include <rsc_types.h>
+#include <pru_virtqueue.h>
+#include <pru_rpmsg.h>
+#include <sys_mailbox.h>
+#include "resource_table_1.h"
+
+volatile register uint32_t __R31;
+
+/* PRU1 is mailbox module user 2 */
+#define MB_USER						2
+/* Mbox0 - mail_u2_irq (mailbox interrupt for PRU1) is Int Number 59 */
+#define MB_INT_NUMBER				59
+
+/* Host-1 Interrupt sets bit 31 in register R31 */
+#define HOST_INT					0x80000000
+
+/* The mailboxes used for RPMsg are defined in the Linux device tree
+ * PRU0 uses mailboxes 2 (From ARM) and 3 (To ARM)
+ * PRU1 uses mailboxes 4 (From ARM) and 5 (To ARM)
+ */
+#define MB_TO_ARM_HOST				5
+#define MB_FROM_ARM_HOST			4
+
+/*
+ * Using the name 'rpmsg-client-sample' will probe the RPMsg sample driver
+ * found at linux-x.y.z/samples/rpmsg/rpmsg_client_sample.c
+ *
+ * Using the name 'rpmsg-pru' will probe the rpmsg_pru driver found
+ * at linux-x.y.z/drivers/rpmsg/rpmsg_pru.c
+ */
+//#define CHAN_NAME					"rpmsg-client-sample"
+#define CHAN_NAME					"rpmsg-pru"
+
+#define CHAN_DESC					"Channel 31"
+#define CHAN_PORT					31
+
+#define LOG_CHAN_DESC					"stdio"
+#define LOG_CHAN_PORT					17
+
+/* 
+ * Used to make sure the Linux drivers are ready for RPMsg communication
+ * Found at linux-x.y.z/include/uapi/linux/virtio_config.h
+ */
+#define VIRTIO_CONFIG_S_DRIVER_OK	4
+
+static int stdoutPort = -1;
+
+struct pru_rpmsg_transport transport;
+
+static void send(const void *data, uint16_t len)
+{
+}
+
+static char hex(uint8_t value)
+{
+  if (value > 0xf) {
+    return '?';
+  }
+  if (value >= 0xa) {
+    return value - 0xa + 'a';
+  }
+  return value + '0';
+}
+
+static void print(char *data, uint16_t value)
+{
+  data[0] = hex((value >> 12) & 0x000f);
+  data[1] = hex((value >> 8) & 0x000f);
+  data[2] = hex((value >> 4) & 0x000f);
+  data[3] = hex((value >> 0) & 0x000f);
+}
+
+static void log(const char *msg)
+{
+  if (stdoutPort < 0) {
+    return;
+  }
+
+  const int len = strlen(msg);
+  pru_rpmsg_send(&transport, LOG_CHAN_PORT, stdoutPort, msg, len);
+}
+
+static void handle(uint16_t src, uint16_t dst, const void *data, uint16_t len)
+{
+  // wie erfahre ich von services der gegenstelle? -> gar nicht
+  //TODO wie kann ich etwas an einen bestimmten empfänger senden?
+  char msg[64];
+  print(&msg[0], src);
+  msg[4] = ' ';
+  msg[5] = '-';
+  msg[6] = '>';
+  msg[7] = ' ';
+  print(&msg[8], dst);
+  msg[12] = ':';
+  msg[13] = ' ';
+  print(&msg[14], len);
+  msg[18] = '\n';
+  msg[19] = '\0';
+  //	send(msg, strlen(msg));
+  pru_rpmsg_send(&transport, dst, src, msg, 20);
+
+  log(msg);
+}
+
+
+
+//XXX wieso muss das global sein? (ev. wegen stack size)
+uint8_t payload[RPMSG_BUF_SIZE];
+
+/*
+ * main.c
+ */
+void main() 
+{
+  uint16_t src, dst, len;
+  volatile uint8_t *status;
+
+  /* allow OCP master port access by the PRU so the PRU can read external memories */
+  CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
+
+  /* clear the status of event MB_INT_NUMBER (the mailbox event) and enable the mailbox event */
+  CT_INTC.SICR_bit.STS_CLR_IDX = MB_INT_NUMBER;
+  CT_MBX.IRQ[MB_USER].ENABLE_SET |= 1 << (MB_FROM_ARM_HOST * 2);
+
+  /* Make sure the Linux drivers are ready for RPMsg communication */
+  status = &resourceTable.rpmsg_vdev.status;
+  while (!(*status & VIRTIO_CONFIG_S_DRIVER_OK));
+
+  /* Initialize pru_virtqueue corresponding to vring0 (PRU to ARM Host direction) */
+  pru_virtqueue_init(&transport.virtqueue0, &resourceTable.rpmsg_vring0, &CT_MBX.MESSAGE[MB_TO_ARM_HOST], &CT_MBX.MESSAGE[MB_FROM_ARM_HOST]);
+
+  /* Initialize pru_virtqueue corresponding to vring1 (ARM Host to PRU direction) */
+  pru_virtqueue_init(&transport.virtqueue1, &resourceTable.rpmsg_vring1, &CT_MBX.MESSAGE[MB_TO_ARM_HOST], &CT_MBX.MESSAGE[MB_FROM_ARM_HOST]);
+
+  /* Create the RPMsg channel between the PRU and ARM user space using the transport structure. */
+  while(pru_rpmsg_channel(RPMSG_NS_CREATE, &transport, CHAN_NAME, CHAN_DESC, CHAN_PORT) != PRU_RPMSG_SUCCESS);
+  while(pru_rpmsg_channel(RPMSG_NS_CREATE, &transport, "logger", LOG_CHAN_DESC, LOG_CHAN_PORT) != PRU_RPMSG_SUCCESS);
+
+  while(1){
+    /* Check bit 31 of register R31 to see if the mailbox interrupt has occurred */
+    if(__R31 & HOST_INT){
+      /* Clear the mailbox interrupt */
+      CT_MBX.IRQ[MB_USER].STATUS_CLR |= 1 << (MB_FROM_ARM_HOST * 2);
+      /* Clear the event status, event MB_INT_NUMBER corresponds to the mailbox interrupt */
+      CT_INTC.SICR_bit.STS_CLR_IDX = MB_INT_NUMBER;
+      /* Use a while loop to read all of the current messages in the mailbox */
+      while(CT_MBX.MSGSTATUS_bit[MB_FROM_ARM_HOST].NBOFMSG > 0){
+        /* Check to see if the message corresponds to a receive event for the PRU */
+        if(CT_MBX.MESSAGE[MB_FROM_ARM_HOST] == 1){
+          /* Receive the message */
+          if(pru_rpmsg_receive(&transport, &src, &dst, payload, &len) == PRU_RPMSG_SUCCESS){
+	    if (dst == LOG_CHAN_PORT) {
+	      stdoutPort = src;
+	    }
+
+            handle(src, dst, payload, len);
+            /* Echo the message back to the same address from which we just received */
+            //						pru_rpmsg_send(&transport, dst, src, payload, len);
+          }
+        }
+      }
+    }
+  }
+}
