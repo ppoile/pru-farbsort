777fae556150e77b04980272aefca5d21bd3e2bc
Matthias Camenzind <matthias.camenzind@bbv.ch>
2017-05-20 11:12:51 +0200

    Revert last commit for regular HW-Setup
    This commit is intended to be used, after the hardware has been fixed and
    the potential crosstalk problem between pulse counter and lightbarrier 1
    has been resolved.
    At that point the wiring can be set back to original state and this commit
    can be used.
    lb1 has to be connected with wires yellow and orange
    lb2 has to be connected with wires blue and green

--- hw/gpio.cpp
+++ hw/gpio.cpp
@@ -8,8 +8,8 @@ const uint32_t Gpio::VALVE3_MASK = 0x20;
 
 // inputs
 const uint32_t Gpio::PULSECOUNTER_MASK = 0x8000;
-const uint32_t Gpio::LIGHTBARRIER1_MASK = 0x10000;
-const uint32_t Gpio::LIGHTBARRIER2_MASK = 0x4000;
+const uint32_t Gpio::LIGHTBARRIER1_MASK = 0x4000;
+const uint32_t Gpio::LIGHTBARRIER2_MASK = 0x10000;
 const uint32_t Gpio::LIGHTBARRIERS3_TO_5_MASK = 0x4;
 
 Gpio::Gpio(uint32_t mask) : mask(mask)
