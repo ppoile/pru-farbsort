03b8062da5ba873bb902617e82f03ff9c0fb55c8
Dominik Berner <dominik.berner@@bbv.ch>
2017-07-03 10:33:34 +0200

    compilation for linux target with new file organisation works
    PRU compilation is not yet moved
    all .o files are located in obj/<mirrorer_path_to.cpp>/<filename>.o
    binaries are put into the bin folder
    
    Added bin and obj to gitignore

--- setup-ti-env.sh
+++ setup-ti-env.sh
@@ -5,4 +5,4 @@ if [ "$0" = "$BASH_SOURCE" ]; then
 fi
 
 export PRU_CGT=~/prg/ti-cgt-pru_2.1.4
-export PRU_LIB=~/prg/ti-processor-sdk-linux-am335x-evm-03.02.00.052.00.05/example-applications/pru-icss-5.1.0
+export PRU_LIB=~/prg/ti-processor-sdk-linux-am335x-evm-03.03.00.04/example-applications/pru-icss-5.1.0
